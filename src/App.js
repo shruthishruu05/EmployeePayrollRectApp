90bdcc290f1ae1eea3860936473f3e8edcaf4dba
shruthishruu05 <shruthi.m@perfios.com>
2021-11-12 00:01:09 +0530

    ADDED : Required dependencies

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 
 import './App.css';
+import PayrollForm from './components/PayrollForm/PayrollForm';
+import { Switch, Router, Route, Redirect } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <p>Hello world !</p>
+      <PayrollForm />
     </div>
   );
 }
+
 
 export default App;
